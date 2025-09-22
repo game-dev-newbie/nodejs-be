324eabb7b180a9057fd52be29aede1ca1b20df7f
game-dev-newbie <bdat6832@gmail.com>
2025-09-22 21:35:28 +0700

    update code

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require("express");
-const app = express();
-const port = 3000;
+const morgan = require("morgan");
+const path = require("path");
+const hbs = require("express-handlebars");
 
+const app = express();
+const port = 8080;
+//Template engine handlebars
+//set extension file .hbs
+app.engine("hbs", hbs.engine({ extname: ".hbs" }));
+app.set("view engine", "hbs");
+//Thư mục chứa file view
+//__dirname: trả về đường dẫn tuyệt đối đến thư mục chứa file hiện tại
+app.set("views", path.join(__dirname, "resources", "views"));
+
+app.use(morgan("combined"));
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
 });
 
 app.listen(port, () => {
